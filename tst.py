6bafdfa08ebd749be52eb9ba0884d38cb6bdfc71
David Vanyan <101475933+DVanyan@users.noreply.github.com>
2023-11-28 12:04:42 +0400

    1

--- tst.py
+++ tst.py
@@ -1,4 +1,18 @@
 import paramiko
+import os
+
+def connect_to_server(server_name):
+    for credential_id in credential_ids:
+        # Получение логина и пароля
+        username = os.getenv(f'USERNAME_{credential_id}')
+        password = os.getenv(f'PASSWORD_{credential_id}')
+
+        # Попытка подключения к серверу
+        success = try_to_connect(server_name, username, password)
+
+        if success:
+            return username, password
+    return None, None
 
 def remove_program(server_ip, username, password, program_name):
     try:
@@ -15,7 +29,8 @@ def remove_program(server_ip, username, password, program_name):
 
         if os_type == 'Linux':
             command = f'sudo apt-get remove {program_name}'  # Пример команды для Ubuntu/Debian
-            # command = f'sudo yum remove {program_name}'  # Пример команды для CentOS/RHEL
+        elif os_type == 'Linux':
+            command = f'sudo yum remove {program_name}'
         elif os_type == 'Windows':
             command = f'wmic product where "name=\'{program_name}\'" call uninstall'  # Команда для удаления программы на Windows
         else:
@@ -38,5 +53,10 @@ def remove_program(server_ip, username, password, program_name):
         # Закрываем SSH-соединение
         ssh_client.close()
 
-# Пример использования функции
-remove_program('192.168.1.100', 'username', 'password', 'zabbix-agent')
+def remove_programs_from_servers(servers, username, password, program_name):
+    for server in servers:
+        remove_program(server, username, password, program_name)
+
+# Запуск удаления приложения на нескольких серверах
+servers = ['192.168.1.100', '192.168.1.101', '192.168.1.102']
+remove_programs_from_servers(servers, 'username', 'password', 'zabbix-agent')
